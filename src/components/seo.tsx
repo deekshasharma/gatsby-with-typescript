566829c48985e90b6196ce26b40592600f496dfa
deekshasharma <deeksha.sharma25@gmail.com>
2020-06-05 08:51:11 -0700

    Added @types/react-helmet dev dependency, SEO is re-written into typescript

---
+++ src/components/seo.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {useStaticQuery, graphql} from "gatsby";
+import {Helmet} from "react-helmet";
+
+type seoProps = {
+    description: string,
+    lang: string,
+    meta: Array<any>,
+    title: string
+}
+
+export const SEO = ({description, lang, meta, title}: seoProps) => {
+    const {site} = useStaticQuery(
+        graphql`
+            query{
+                site {
+                    siteMetadata {
+                        description
+                        title
+                    }
+                }
+            }
+        `
+    )
+    const metaDescription = description || site.siteMetadata.description;
+    return <Helmet htmlAttributes={{lang}}
+                   title={title}
+                   titleTemplate={`%s | ${site.siteMetadata.title}`}
+                   meta={[
+                       {
+                           name: `description`,
+                           content: metaDescription,
+                       },
+                       {
+                           property: `og:title`,
+                           content: title,
+                       },
+                       {
+                           property: `og:description`,
+                           content: metaDescription,
+                       },
+                       {
+                           property: `og:type`,
+                           content: `website`,
+                       },
+                       {
+                           name: `twitter:card`,
+                           content: `summary`,
+                       },
+                       {
+                           name: `twitter:creator`,
+                           content: site.siteMetadata.author,
+                       },
+                       {
+                           name: `twitter:title`,
+                           content: title,
+                       },
+                       {
+                           name: `twitter:description`,
+                           content: metaDescription,
+                       },
+                   ].concat(meta)}
+    />
+
+}
